4d26e212bf1fd8671f395be731bbb047444c1189
selfim <lm233427@gmail.com>
2017-01-15 17:01:42 +0800

    '入口文件'

---
+++ core/main.php
@@ -0,0 +1,9 @@
+<?php
+namespace core;
+
+class main
+{
+    public static function run() {
+        p('ok');
+    }
+}
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * 入口文件
+ * 1.定义常量
+ * 2.加载函数库
+ * 3.启动框架
+ */
+define('ROOT',dirname(realpath(__FILE__)));
+define('CORE',ROOT.'/core');
+define('APP',ROOT.'/app');
+
+define('DEBUG',true);
+if (DEBUG){
+    ini_set('display_errors','On');
+}else {
+    ini_set('display_errors','Off');
+}
+
+require CORE.'/common/functions.php';
+require CORE.'/main.php';
+\core\main::run();
